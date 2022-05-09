af2ceda7b92cd8a854022ec35801ddd5055f9b5a
rafaeltiribas <86385916+rafaeltiribas@users.noreply.github.com>
2022-05-09 20:34:22 -0300

    Update contref.c

--- contref.c
+++ contref.c
@@ -2,25 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-contador *lista = NULL;
-
-void *malloc2(int tamanho)
+int main(int argc, char const *argv[])
 {
-    contador *aux;
-    aux = malloc2(sizeof(contador));
-    aux->endereco = malloc(tamanho);
-    aux->cont = 1;
-    aux->prox = NULL;
-    if(lista!=NULL)
-        aux->prox = lista;
-    lista = aux;
-    return lista->endereco;
+    int *v = malloc2(sizeof(int));
+    *v = 10;
+    int *w = malloc2(sizeof(int));
+    dump();
+    *w = 20;
+    atrib2(v, w);
+    dump();
+    char *c = malloc2(sizeof(char));
+    *c = 'Z';
+    dump();
+    atrib2(w, NULL);
+    dump();
+    return 0;
 }
 
-void atualiza(void *end1, int incremento)
-{
-
-}
 
 
 
