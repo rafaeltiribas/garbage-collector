a2d42a8ed03e55275c63b6f1daa8c42655c8310a
rafaeltiribas <86385916+rafaeltiribas@users.noreply.github.com>
2022-05-09 20:33:51 -0300

    Update contref.h

--- contref.h
+++ contref.h
@@ -1,5 +1,5 @@
-#ifndef CONTREF_H_INCLUDED
-#define CONTREF_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct contador
 {
@@ -8,10 +8,69 @@ typedef struct contador
     struct contador *prox;
 }contador;
 
-void *malloc2(int tamanho);
-void *atrib2(void **end1, void *end2);
-contador *removeElemento(contador *heap, void *endereco);
-void dump();
-void atualiza(void *end1, int incremento);
+contador* c = NULL;
 
-#endif
+void* malloc2(int tamanho)  //realiza a alocacao dinamica e retorna o endereco da area criada
+{
+    contador* aux = (contador*)malloc(sizeof(contador));
+    aux->endereco = malloc(tamanho);
+    aux->cont = 1;
+    if(c == NULL)
+        c = aux;
+    else
+        c->prox = aux;
+
+    return c->endereco;
+}
+
+contador* apagaNo(void* endereco1)
+{
+    contador* aux = c;
+    while(aux!=NULL&&aux->endereco!=endereco1)
+        aux = aux->prox;
+    if(aux!=NULL)
+    {
+        free(aux->endereco);
+        free(aux);
+        return NULL;
+    }
+}
+
+void atualiza(void* endereco1, void* endereco2)
+{
+    int i = 0;
+    contador* aux = c;
+    while(aux!=NULL && aux->endereco!=endereco1)
+        aux = aux->prox;
+    if(aux!=NULL&&aux->endereco==endereco1)
+        aux->cont--;
+    if(aux->cont == 0)
+        apagaNo(endereco1);
+    aux = c;
+    while(aux!=NULL && aux->endereco!=endereco2)
+        aux = aux->prox;
+    if(aux!=NULL&&aux->endereco==endereco1)
+        aux->cont++;
+}
+
+void atrib2(void* endereco1, void* endereco2)
+{
+    atualiza(endereco1, endereco2);
+    endereco1 = endereco2;
+}
+
+void dump()
+{
+    contador* aux = c;
+    if(aux==NULL)
+        printf("Sem alocacao de memoria.\n");
+    else
+    {
+       while(aux!=NULL)
+       {
+           printf("Endereco: %p\nContador: %d\n", aux->endereco, aux->cont);
+           aux=aux->prox;
+       }
+    }
+}
+
